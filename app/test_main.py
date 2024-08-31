7937a0733538f46cf9e52c98a6ce2ff3559f8dad
Rudge0 <maksim.pariychuk07@gmail.com>
2024-08-31 11:55:28 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,33 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+def test_argument_should_not_be_negative() -> None:
+    for el in get_coin_combination(-3):
+        assert el < 0
+
+
+@pytest.mark.parametrize(
+    "actual,expected,",
+[
+    (
+        1,
+        [1, 0, 0, 0]
+    ),
+    (
+        6,
+        [1, 1, 0, 0]
+    ),
+    (
+        17,
+        [2, 1, 1, 0]
+    ),
+    (
+        50,
+        [0, 0, 0, 2]
+    )
+    ]
+)
+def test_putput_is_equal_to_expected(actual, expected) -> None:
+    assert get_coin_combination(actual) == expected
+
